c2227241fb49e37413deca0aff29c0237ffbc594
Marc Carné <marc.carne@werlabs.com>
2019-05-18 23:48:31 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# arduino-auto-watering
+Watering system used for my balcony. It uses an aquary water pump and a soil moist sensor. The soil moist sensor is pulsed for measuring to prevent electrolisis destruction of the sensor, or better said, to prolong the sensor life. This is done simply with a 437 transistor.
---
+++ pumpSketch.ino
@@ -0,0 +1,56 @@
+// Pins definition
+int moistureSensorPin = 0;
+int pumpPin = 7;
+int moistureActivationPin = 5;
+
+// Constants
+const int MOISTURE_SENSOR_DRY = 700;
+const int PUMPING_TIME = 10000;
+const int AWAIT_TIME = 20000;
+
+// Variables
+int moistureSensorValue;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(pumpPin, OUTPUT);
+  pinMode(moistureActivationPin, OUTPUT);
+  
+  digitalWrite(pumpPin, HIGH);
+  digitalWrite(moistureActivationPin, LOW);
+
+  delay(2000);
+}
+
+void loop() {
+  moistureSensorValue = readMoistureSensor();
+  if (moistureSensorValue >= MOISTURE_SENSOR_DRY) {
+    // Dry
+    startPumpingWater();
+    delay(PUMPING_TIME);
+    stopPumpingWater();
+    delay(AWAIT_TIME);
+  } else {
+    // Should not measure again for a while
+    delay(AWAIT_TIME);
+  }
+}
+
+void startPumpingWater() {
+  digitalWrite(pumpPin, LOW);
+}
+
+void stopPumpingWater() {
+  digitalWrite(pumpPin, HIGH);
+}
+
+int readMoistureSensor() {
+  int value;
+  digitalWrite(moistureActivationPin, HIGH);
+  delay(1000);
+  value = analogRead(moistureSensorPin);
+  delay(1000);
+  digitalWrite(moistureActivationPin, LOW);
+  return value;
+}
